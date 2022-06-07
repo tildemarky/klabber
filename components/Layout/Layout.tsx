4f164d8439467b789e3f3e4961e161281054916b
Marky R <99497246+tildemarky@users.noreply.github.com>
2022-06-07 11:30:52 -0400

    add umami stats

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { useLang } from "@hooks/useLang";
 import { common } from "@locales/common";
+import Script from "next/script";
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -12,6 +13,13 @@ function Layout({ children }: LayoutProps): JSX.Element {
 
   return (
     <>
+      <Script
+        async
+        defer
+        data-website-id="f06a2b69-f4ee-432c-97ec-56a1d2e410ae"
+        src="https://umamiocto.netlify.app/umami.js"
+        strategy="afterInteractive"
+      />
       <Header title={common[lang].title} disabled />
 
       {children}
