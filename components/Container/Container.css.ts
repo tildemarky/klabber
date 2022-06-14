5590f0e01f2acf73df02d0b180db430af1876631
Marky R <99497246+tildemarky@users.noreply.github.com>
2022-06-14 19:14:13 -0400

    fix the very wide suggestion box

--- components/Container/Container.css.ts
+++ components/Container/Container.css.ts
@@ -7,6 +7,11 @@ const StyledContainer = style({
   justifyContent: "flex-end",
   margin: "0 auto",
   width: "100%",
+  "@media": {
+    "screen and (min-width: 700px)": {
+      width: "60vw",
+    },
+  },
 });
 
 export default StyledContainer;
